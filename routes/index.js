91f925ef496a9960dc9ffef0858f32ed02653489
QuentinLB <quentinlebesnerais@Mini-de-Quentin.concorde-pereire.loc>
2021-04-30 14:40:23 +0200

    delete wishlist en bdd opérationel

--- routes/index.js
+++ routes/index.js
@@ -104,5 +104,15 @@ var userSaved = await user.save()
 res.json(userSaved)
 })
 
+router.delete('/delete-article', async function(req,res,next){
+  var user = await userModel.findOne({token:req.body.token})
+  console.log(user.articles)
+  var tempTab = user.articles
+  var newTab = tempTab.filter(x=>x.title!==req.body.title)
+  user.articles = newTab
+  await user.save()
+res.json("article supprimé")
+})
+
 
 module.exports = router;
